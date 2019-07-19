2cbc663efac4fb346ca0112dcd28916e68f4732f
optimistic-writer-7178 <duot.jim@gmail.com>
2019-07-19 22:20:05 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,9 @@
 def reverse_each_word sentence
-  =begin
-  word_array = []
-  sentence.split(' ').each { |x| word_array << x.reverse}
-  word_array.join ' '
-  =end
+  #=begin
+  #word_array = []
+  #sentence.split(' ').each { |x| word_array << x.reverse}
+  #word_array.join ' '
+  #=end
 
   sentence.split(' ').collect { |x| x.reverse}.join(' ')
 end
